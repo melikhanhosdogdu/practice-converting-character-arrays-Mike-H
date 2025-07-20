bd7a07328e7e803871009ad169f7276e8d1c3f27
Mike Melikhan H <62219260+melikhanhosdogdu@users.noreply.github.com>
2025-07-20 16:59:18 -0500

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let decoded1 = message1.split('').reverse().join('');
+console.log(decoded1);
+
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let decoded2 = message2.split('').reverse().join('');
+console.log(decoded2);
+
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let decoded3 = message3.split('').reverse().join('');
+console.log(decoded3);
+
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+let decoded4 = message4.split('').reverse().join('');
+console.log(decoded4);
+
+let myMessage1 = "You are learning fast!";
+let reverse1 = myMessage1.split('').reverse().join('');
+console.log(reverse1);
+
+let myMessage2 = "Just keep building.";
+let reverse2 = myMessage2.split('').reverse().join('');
+console.log(reverse2);
